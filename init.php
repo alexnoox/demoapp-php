d3328608e6121d54f9150e7b8df479f3557d0d72
Alexandre Jarnoux <alex.jarnoux@gmail.com>
2016-06-20 18:23:14 +1000

    [DEVPL-36] Init via the dev-platform and add an other environment via a locale file

--- init.php
+++ init.php
@@ -2,7 +2,6 @@
 define('ROOT_DIR',dirname(__FILE__));
 require ROOT_DIR . '/vendor/autoload.php';
 
-// Configure Maestrano API
-Maestrano::configure(ROOT_DIR . "/maestrano.json");
-
-echo "Maestrano::fetchEndpointsConfig", Maestrano::fetchEndpointsConfig();
+// Configure Maestrano API using the dev-platform
+Maestrano::autoConfigure(ROOT_DIR . "/dev-platform.json");
+Maestrano::with('maestrano')->configure(ROOT_DIR . "/maestrano.json");
