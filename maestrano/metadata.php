9d79035ad73d75fda621a5157d586eec0228548a
Alexandre Jarnoux <alex.jarnoux@gmail.com>
2016-06-24 17:21:53 +1000

    Multi-marketplace metadata endpoint

--- maestrano/metadata.php
+++ maestrano/metadata.php
@@ -2,9 +2,13 @@
   require_once '../init.php';
   header('Content-Type: application/json');
 
-//  if (Maestrano::authenticate($_SERVER['PHP_AUTH_USER'],$_SERVER['PHP_AUTH_PW'])) {
-//    echo Maestrano::with('maestrano-dev')->toMetadata();
-//  } else {
-//    echo "Sorry! I'm not giving you my API metadata";
-//  }
-echo Maestrano::with('maestrano-dev')->toMetadata();
+  if (Maestrano::authenticate($_SERVER['PHP_AUTH_USER'],$_SERVER['PHP_AUTH_PW'])) {
+    if (array_key_exists('marketplace', $_GET)) {
+      $marketplace = $_GET['marketplace'];
+      echo Maestrano::with($marketplace)->toMetadata();
+    } else {
+      echo 'Marketplace get param is missing';
+    }
+  } else {
+    echo "Sorry! I'm not giving you my API metadata";
+  }
