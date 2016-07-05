211cd0b8116eaff94a5a32c7919f3145fc1b0a3f
Alexandre Jarnoux <alex.jarnoux@gmail.com>
2016-07-05 12:10:29 +1000

    Enable config using environment variables

--- init.php
+++ init.php
@@ -3,5 +3,5 @@ define('ROOT_DIR', dirname(__FILE__));
 require ROOT_DIR . '/vendor/autoload.php';
 
 // Configure Maestrano API using the dev-platform
-Maestrano::autoConfigure(ROOT_DIR . "/dev-platform.json");
+Maestrano::autoConfigure();
 Maestrano::with('maestrano')->configure(ROOT_DIR . "/maestrano.json");
