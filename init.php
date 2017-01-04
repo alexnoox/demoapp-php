6ba64f1f1e890a3ed33edc5476e6807ba25e1c60
x4d3 <git@xade.eu>
2017-01-04 11:54:25 +0000

    Update dev platform url and add example using file

--- init.php
+++ init.php
@@ -2,5 +2,7 @@
 define('ROOT_DIR', dirname(__FILE__));
 require ROOT_DIR . '/vendor/autoload.php';
 
-// Configure Maestrano API using the dev-platform
+// Configure Maestrano API using the dev-platform, using the environment variables
 Maestrano::autoConfigure();
+//If you wish to use a file dev-platform.json use this syntax
+//Maestrano::autoConfigure(ROOT_DIR.'/dev-platform.json');
